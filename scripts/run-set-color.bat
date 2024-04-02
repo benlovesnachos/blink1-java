7be22ad2f8471e085612307e573cfaade3adb54d
Benjamin Rodriguez <ben@benlovesnachos.com>
2024-04-02 13:19:22 -0500

    Add scripts for changing color based on a status. - `stat_available.bat` set to color green. - `stat_busy.bat` set to color blue. - `stat_dnd.bat` set to color red. - `stat_gone.bat` turn color off.
    Add script for setting the color without specifying the OnOffColor class.

---
+++ scripts/run-set-color.bat
@@ -0,0 +1,6 @@
+@echo off
+
+java ^
+ -Djava.awt.headless=true ^
+  -cp ..\blink1-library\target\blink1-library-jar-with-dependencies.jar;..\blink1-java-examples\target\blink1-java-examples.jar ^
+   com.thingm.blink1.OnOffColor %1 %2 %3 %4
